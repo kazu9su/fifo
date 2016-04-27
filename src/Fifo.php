31a4e5f6c183d3c9c204e63103d3be7ab109a31c
Tomy <kazushige.tominaga@mkt-apps.com>
2016-04-27 13:58:30 +0900

    update

--- src/Fifo.php
+++ src/Fifo.php
@@ -2,29 +2,58 @@
 
 namespace fifo;
 
+/**
+ * Class Fifo
+ * @package fifo
+ */
 class Fifo
 {
+    /**
+     * @var int
+     */
     protected $capacity;
 
+    /**
+     * @var array
+     */
     protected $cache = [];
 
+    /**
+     * @var array
+     */
     protected $order = [];
 
+    /**
+     * Fifo constructor.
+     * @param int $capacity
+     */
     public function __construct($capacity)
     {
         $this->capacity = $capacity;
     }
 
+    /**
+     * @param string $key
+     * @return bool
+     */
     protected function exists($key)
     {
         return isset($this->cache[$key]);
     }
 
+    /**
+     * @param string $key
+     * @return mixed|null
+     */
     public function get($key)
     {
         return $this->exists($key) ? $this->cache[$key] : null;
     }
 
+    /**
+     * @param string $key
+     * @param mixed|null $value
+     */
     public function put($key, $value)
     {
         if ($this->overCapacity()) {
@@ -35,6 +64,10 @@ class Fifo
         $this->cache[$key] = $value;
     }
 
+    /**
+     * @param string $key
+     * @return bool
+     */
     public function remove($key)
     {
         if ($this->exists($key)) {
@@ -46,6 +79,9 @@ class Fifo
         return false;
     }
 
+    /**
+     * @return bool
+     */
     protected function overCapacity()
     {
         return count($this->order) + 1 > $this->capacity;
